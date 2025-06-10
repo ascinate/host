f70b92d0a0c8d769a3205cd2f05b2041f9ecc5fe
subrata <subrata@ascinatetech.com>
2025-06-10 16:45:40 +0530

    page css done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,33 +7,44 @@ import Navbar from '../components/Navbar'
 
 
 //Image
-import oone from "/o.png"
-import otwo from "/o2.png"
-import othree from "/o3.png"
-import ofour from "/o4.png"
+import ione from "/o.png";
+import ithree from "/o2.png";
+import ifour from "/o3.png";
+import isix from "/o4.png";
+import CustomCursor from '../components/CustomCursor';
 
 
 function Home() {
     return (
         <>
+            <CustomCursor/>
             <Navbar />
-            <main>
+            <main className='float-left w-full'>
                 <section className='section-hero'>
 
                     <div className='wrapper'>
-                        <div className='container-hero flex items-center gap-52 '>
-                            <div>
-                                <h1 className='hero-main-text'>Hosting and oracle services with Hosts</h1>
+                        <div className='container-hero relative'>
+                            <div className='banner-tms01'>
+                                <h1 className='hero-main-text'>Hosting and <span> oracle </span> <strong> Services </strong> with Hosts</h1>
                                 <p className='sub-text mb-11'>Decentralizes hosting and oracle services by aggregating on and off-chain data.</p>
                                 <button className='theme-color-btn' id=''>Get Started</button>
                                 <button className='white-color-btn ' id=''>Try Demo</button>
                             </div>
-                            <div className='hero-img'>
-                                <img src={othree} alt="" className='othree'/>
-                                <img src={ofour} alt="" className='ofour'/>
-                                <img src={oone} alt="" className='oone'/>
-                                <img src={otwo} alt="" className='otwo'/>
+                           
+
+                            <div className='ithree'>
+                                <img src={ithree} alt="b" />
                             </div>
+                            <div className='ifour'>
+                                <img src={ifour} alt="b" />
+                            </div>
+                            <div className='ione-face'>
+                                <img src={ione} alt="g" />
+                            </div>
+                            <div className='isix'>
+                                <img src={isix} alt="h" />
+                            </div>
+                            
                         </div>
                     </div>
                 </section>
